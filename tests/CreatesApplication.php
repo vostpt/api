fa988b56fc427b24a44249bfe3114fd5bf6c3ab1
Quetzy Garcia <quetzyg@altek.org>
2019-04-08 09:50:17 +0100

    fix(CreatesApplication): maintain compatibility

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace VOSTPT\Tests;
 
 use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Foundation\Application;
 
 trait CreatesApplication
 {
@@ -14,7 +13,7 @@ trait CreatesApplication
      *
      * @return \Illuminate\Foundation\Application
      */
-    public function createApplication(): Application
+    public function createApplication()
     {
         $app = require __DIR__.'/../bootstrap/app.php';
 
