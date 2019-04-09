800aff88fd7b73702b6c4045aac9538d9892a7aa
Quetzy Garcia <quetzyg@altek.org>
2019-04-09 14:28:22 +0100

    fix(Application): SessionServiceProvider is required for validation purposes

--- config/app.php
+++ config/app.php
@@ -155,6 +155,7 @@ return [
         Illuminate\Pipeline\PipelineServiceProvider::class,
         Illuminate\Queue\QueueServiceProvider::class,
         Illuminate\Redis\RedisServiceProvider::class,
+        Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
