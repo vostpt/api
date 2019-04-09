56b6280c09b9d5ba9d809d380a42c42f5ca7bce1
Quetzy Garcia <quetzyg@altek.org>
2019-04-09 14:20:56 +0100

    feat(Users): add serializer class

---
+++ app/Http/Serializers/UserSerializer.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Http\Serializers;
+
+use Tobscure\JsonApi\AbstractSerializer;
+use Tobscure\JsonApi\Collection;
+use Tobscure\JsonApi\Relationship;
+use VOSTPT\Models\User;
+
+class UserSerializer extends AbstractSerializer
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $type = 'users';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getLinks($model): array
+    {
+        return [
+            'self' => route('users::view', [
+                'user' => $model->id,
+            ]),
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAttributes($model, array $fields = null): array
+    {
+        return [
+            'email'      => $model->email,
+            'name'       => $model->name,
+            'surname'    => $model->surname,
+            'created_at' => $model->created_at->toDateTimeString(),
+            'updated_at' => $model->updated_at->toDateTimeString(),
+        ];
+    }
+
+    /**
+     * Associated Roles.
+     *
+     * @param User $user
+     *
+     * @return \Tobscure\JsonApi\Relationship
+     */
+    public function roles(User $user): Relationship
+    {
+        return new Relationship(new Collection($user->roles()->get(), new RoleSerializer()));
+    }
+}
