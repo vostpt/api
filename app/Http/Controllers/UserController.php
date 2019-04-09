a235d133a29b5ad7d1b5d9474e4a1672887e503b
Quetzy Garcia <quetzyg@altek.org>
2019-04-09 14:38:20 +0100

    feat(Users): initial controller implementation

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Http\Controllers\User;
+
+use Illuminate\Http\JsonResponse;
+use VOSTPT\Filters\Contracts\UserFilter;
+use VOSTPT\Http\Controllers\Controller;
+use VOSTPT\Http\Requests\User\Index;
+use VOSTPT\Http\Requests\User\View;
+use VOSTPT\Http\Serializers\UserSerializer;
+use VOSTPT\Models\User;
+use VOSTPT\Repositories\Contracts\UserRepository;
+
+class UserController extends Controller
+{
+    /**
+     * Index Users.
+     *
+     * @param Index          $request
+     * @param UserFilter     $filter
+     * @param UserRepository $userRepository
+     *
+     * @throws \InvalidArgumentException
+     * @throws \OutOfBoundsException
+     * @throws \RuntimeException
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Index $request, UserFilter $filter, UserRepository $userRepository): JsonResponse
+    {
+        $filter->setSortColumn($request->input('sort', $filter->getSortColumn()))
+            ->setSortOrder($request->input('order', $filter->getSortOrder()))
+            ->setPageNumber((int) $request->input('page.number', 1))
+            ->setPageSize((int) $request->input('page.size', 10));
+
+        if ($search = $request->input('search')) {
+            $filter->withSearch($search);
+        }
+
+        if ($roles = $request->input('roles')) {
+            $filter->withRoles($roles);
+        }
+
+        $paginator = $this->createPaginator(User::class, $userRepository->createQueryBuilder(), $filter);
+
+        return response()->paginator($paginator, new UserSerializer());
+    }
+
+    /**
+     * View a User.
+     *
+     * @param View $request
+     * @param User $user
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function view(View $request, User $user): JsonResponse
+    {
+        return response()->resource($user, new UserSerializer(), [
+            'roles',
+        ]);
+    }
+}
