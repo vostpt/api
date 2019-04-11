276ccee99f83b4f4876f6057a7b5add79f8e05d0
Quetzy Garcia <quetzyg@altek.org>
2019-04-11 14:26:26 +0100

    feat(Districts): implement serializer class

---
+++ app/Http/Serializers/DistrictSerializer.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Http\Serializers;
+
+use Tobscure\JsonApi\AbstractSerializer;
+
+class DistrictSerializer extends AbstractSerializer
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $type = 'districts';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getLinks($model): array
+    {
+        return [
+            'self' => route('districts::view', [
+                'district' => $model->id,
+            ]),
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAttributes($model, array $fields = null): array
+    {
+        return [
+            'code'       => $model->code,
+            'name'       => $model->name,
+            'created_at' => $model->created_at->toDateTimeString(),
+            'updated_at' => $model->updated_at->toDateTimeString(),
+        ];
+    }
+}
