58ad7a3ba04a1a46037ff7893028041ce0350928
Quetzy Garcia <quetzyg@altek.org>
2019-04-13 01:02:39 +0100

    feat(Acronyms): add view/index request classes

---
+++ app/Http/Requests/Acronym/Index.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Http\Requests\Acronym;
+
+use Illuminate\Validation\Rule;
+use VOSTPT\Filters\AcronymFilter;
+use VOSTPT\Http\Requests\Request;
+
+class Index extends Request
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function rules(): array
+    {
+        return [
+            'page.number' => [
+                'integer',
+            ],
+            'page.size' => [
+                'integer',
+            ],
+            'search' => [
+                'string',
+            ],
+            'sort' => [
+                Rule::in(AcronymFilter::getSortableColumns()),
+            ],
+            'order' => [
+                Rule::in(AcronymFilter::getOrderValues()),
+            ],
+        ];
+    }
+}
---
+++ app/Http/Requests/Acronym/View.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Http\Requests\Acronym;
+
+use VOSTPT\Http\Requests\Request;
+
+class View extends Request
+{
+    //
+}
