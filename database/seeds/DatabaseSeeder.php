9dcffc50e0ba3cd36a3a958620989504b659d863
Quetzy Garcia <quetzyg@altek.org>
2019-04-13 02:16:31 +0100

    feat(Acronyms): add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(DistrictSeeder::class);
         $this->call(CountySeeder::class);
         $this->call(ParishSeeder::class);
+        $this->call(AcronymSeeder::class);
     }
 }
