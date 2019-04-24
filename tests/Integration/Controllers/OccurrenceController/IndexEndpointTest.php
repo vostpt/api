40c610fb1807055930d6a508520717ea5d6652b0
Quetzy Garcia <quetzyg@altek.org>
2019-04-24 19:22:15 +0100

    test(Occurrences): add coverage for index/view endpoints

---
+++ tests/Integration/Controllers/OccurrenceController/IndexEndpointTest.php
@@ -0,0 +1,178 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Tests\Integration\Controllers\OccurrenceController;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use VOSTPT\Models\Event;
+use VOSTPT\Models\Occurrence;
+use VOSTPT\Models\Parish;
+use VOSTPT\Models\ProCivOccurrence;
+use VOSTPT\Tests\Integration\TestCase;
+
+class IndexEndpointTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function itFailsToIndexOccurrencesDueToInvalidContentTypeHeader(): void
+    {
+        $response = $this->json('GET', route('occurrences::index'));
+
+        $response->assertHeader('Content-Type', 'application/vnd.api+json');
+        $response->assertStatus(415);
+        $response->assertJson([
+            'errors' => [
+                [
+                    'status' => 415,
+                    'detail' => 'Wrong media type',
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function itFailsToIndexOccurrencesDueToValidation(): void
+    {
+        $response = $this->json('GET', route('occurrences::index'), [
+            'page' => [
+                'number' => 'second',
+                'size'   => 'ten',
+            ],
+            'search' => '',
+            'events' => [
+                1,
+            ],
+            'parishes' => [
+                1,
+            ],
+            'sort'  => 'id',
+            'order' => 'up',
+        ], [
+            'Content-Type' => 'application/vnd.api+json',
+        ]);
+
+        $response->assertHeader('Content-Type', 'application/vnd.api+json');
+        $response->assertStatus(422);
+        $response->assertJson([
+            'errors' => [
+                [
+                    'detail' => 'The page.number must be an integer.',
+                    'meta'   => [
+                        'field' => 'page.number',
+                    ],
+                ],
+                [
+                    'detail' => 'The page.size must be an integer.',
+                    'meta'   => [
+                        'field' => 'page.size',
+                    ],
+                ],
+                [
+                    'detail' => 'The search must be a string.',
+                    'meta'   => [
+                        'field' => 'search',
+                    ],
+                ],
+                [
+                    'detail' => 'The selected sort is invalid.',
+                    'meta'   => [
+                        'field' => 'sort',
+                    ],
+                ],
+                [
+                    'detail' => 'The selected order is invalid.',
+                    'meta'   => [
+                        'field' => 'order',
+                    ],
+                ],
+                [
+                    'detail' => 'The selected events.0 is invalid.',
+                    'meta'   => [
+                        'field' => 'events.0',
+                    ],
+                ],
+                [
+                    'detail' => 'The selected parishes.0 is invalid.',
+                    'meta'   => [
+                        'field' => 'parishes.0',
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function itSuccessfullyIndexesOccurrences(): void
+    {
+        $event  = factory(Event::class)->create();
+        $parish = factory(Parish::class)->create();
+
+        $occurrences = factory(Occurrence::class, 20)->make([
+            'event_id'  => $event->getKey(),
+            'parish_id' => $parish->getKey(),
+        ]);
+
+        factory(ProCivOccurrence::class, 20)->create()->each(function (ProCivOccurrence $proCivOccurrence, $index) use ($occurrences) {
+            $proCivOccurrence->occurrence()->save($occurrences[$index]);
+        });
+
+        $response = $this->json('GET', route('occurrences::index'), [
+            'page' => [
+                'number' => 2,
+                'size'   => 2,
+            ],
+            'events' => [
+                $event->getKey(),
+            ],
+            'parishes' => [
+                $parish->getKey(),
+            ],
+            'search' => '0 1 2 3 4 5 6 7 8 9',
+            'sort'   => 'locality',
+            'order'  => 'asc',
+        ], [
+            'Content-Type' => 'application/vnd.api+json',
+        ]);
+
+        $response->assertHeader('Content-Type', 'application/vnd.api+json');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'links' => [
+                'first',
+                'last',
+                'prev',
+                'next',
+            ],
+            'data' => [
+                '*' => [
+                    'type',
+                    'id',
+                    'attributes' => [
+                        'locality',
+                        'latitude',
+                        'longitude',
+                        'started_at',
+                        'ended_at',
+                        'created_at',
+                        'updated_at',
+                    ],
+                    'links' => [
+                        'self',
+                    ],
+                ],
+            ],
+            'meta' => [
+                'per_page',
+                'total',
+            ],
+        ]);
+    }
+}
---
+++ tests/Integration/Controllers/OccurrenceController/ViewEndpointTest.php
@@ -0,0 +1,158 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Tests\Integration\Controllers\OccurrenceController;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use VOSTPT\Models\Occurrence;
+use VOSTPT\Models\ProCivOccurrence;
+use VOSTPT\Tests\Integration\TestCase;
+
+class ViewEndpointTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function itFailsToViewCountyDueToInvalidContentTypeHeader(): void
+    {
+        $response = $this->json('GET', route('occurrences::view', [
+            'occurrence' => 1,
+        ]));
+
+        $response->assertHeader('Content-Type', 'application/vnd.api+json');
+        $response->assertStatus(415);
+        $response->assertJson([
+            'errors' => [
+                [
+                    'status' => 415,
+                    'detail' => 'Wrong media type',
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function itFailsToViewCountyDueToRecordNotFound(): void
+    {
+        $response = $this->json('GET', route('occurrences::view', [
+            'occurrence' => 1,
+        ]), [], [
+            'Content-Type' => 'application/vnd.api+json',
+        ]);
+
+        $response->assertHeader('Content-Type', 'application/vnd.api+json');
+        $response->assertStatus(404);
+        $response->assertJson([
+            'errors' => [
+                [
+                    'status' => 404,
+                    'detail' => 'Occurrence Not Found',
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function itSuccessfullyViewsCounty(): void
+    {
+        $occurrence       = factory(Occurrence::class)->make();
+        $proCivOccurrence = factory(ProCivOccurrence::class)->create();
+        $proCivOccurrence->occurrence()->save($occurrence);
+
+        $response = $this->json('GET', route('occurrences::view', [
+            'occurrence' => $occurrence->getKey(),
+        ]), [], [
+            'Content-Type' => 'application/vnd.api+json',
+        ]);
+
+        $response->assertHeader('Content-Type', 'application/vnd.api+json');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [
+                'type',
+                'id',
+                'attributes' => [
+                    'locality',
+                    'latitude',
+                    'longitude',
+                    'started_at',
+                    'ended_at',
+                    'created_at',
+                    'updated_at',
+                ],
+                'relationships' => [
+                    'event' => [
+                        'data' => [
+                            'type',
+                            'id',
+                        ],
+                    ],
+                    'parish' => [
+                        'data' => [
+                            'type',
+                            'id',
+                        ],
+                    ],
+                ],
+                'links' => [
+                    'self',
+                ],
+            ],
+            'included' => [
+                [
+                    'type',
+                    'id',
+                    'attributes' => [
+                        'name',
+                        'description',
+                        'latitude',
+                        'longitude',
+                        'started_at',
+                        'ended_at',
+                        'created_at',
+                        'updated_at',
+                    ],
+                    'links' => [
+                        'self',
+                    ],
+                ],
+                [
+                    'type',
+                    'id',
+                    'attributes' => [
+                        'code',
+                        'name',
+                        'created_at',
+                        'updated_at',
+                    ],
+                    'links' => [
+                        'self',
+                    ],
+                ],
+                [
+                    'type',
+                    'id',
+                    'attributes' => [
+                        'remote_id',
+                        'ground_assets_involved',
+                        'ground_operatives_involved',
+                        'aerial_assets_involved',
+                        'aerial_operatives_involved',
+                        'created_at',
+                        'updated_at',
+                    ],
+                    'links' => [
+                        'self',
+                    ],
+                ],
+            ],
+        ]);
+    }
+}
