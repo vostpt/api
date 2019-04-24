9cb9ded97e0b12d2f0f44b5d9b388274d114bab9
Quetzy Garcia <quetzyg@altek.org>
2019-04-24 22:32:22 +0100

    feat(ProCivOccurrences): add controller and register view endpoint

--- routes/api/occurrences.php
+++ routes/api/occurrences.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use VOSTPT\Http\Controllers\OccurrenceController;
+use VOSTPT\Http\Controllers\ProCivOccurrenceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,11 @@ Route::prefix('v1/occurrences')->name('occurrences::')->group(function () {
     Route::get('/', [
         'as'   => 'index',
         'uses' => OccurrenceController::class.'@index',
+    ]);
+
+    Route::get('/prociv/{ProCivOccurrence}', [
+        'as'   => 'prociv::view',
+        'uses' => ProCivOccurrenceController::class.'@view',
     ]);
 
     Route::get('/{Occurrence}', [
