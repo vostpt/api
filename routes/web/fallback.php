241f78a26baa3d6de35328a25f68b5002e6d1d71
Quetzy Garcia <quetzyg@altek.org>
2019-04-25 16:48:22 +0100

    chore(Routes): remove unnecessary documentation route name

--- routes/web/fallback.php
+++ routes/web/fallback.php
@@ -5,5 +5,4 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use VOSTPT\Http\Controllers\FallbackController;
 
-Route::fallback([FallbackController::class, 'documentation'])
-    ->name('documentation');
+Route::fallback([FallbackController::class, 'documentation']);
