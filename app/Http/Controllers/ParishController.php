2dff2e1ad4cc266e0894d1716154c2154980a604
Quetzy Garcia <quetzyg@altek.org>
2019-05-16 14:23:02 +0100

    feat(Controllers): pass exact match input value when doing a search

--- app/Http/Controllers/ParishController.php
+++ app/Http/Controllers/ParishController.php
@@ -35,7 +35,7 @@ class ParishController extends Controller
             ->setPageSize((int) $request->input('page.size', $filter->getPageSize()));
 
         if ($request->has('search')) {
-            $filter->withSearch($request->input('search'));
+            $filter->withSearch($request->input('search'), (bool) $request->input('exact', false));
         }
 
         if ($request->has('counties')) {
