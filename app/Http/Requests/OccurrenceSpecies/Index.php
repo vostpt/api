7eeab8a33c2ada1b2d8d01190ec4e7a3788b3448
Quetzy Garcia <quetzyg@altek.org>
2019-05-24 21:29:26 +0100

    feat(OccurrenceSpecies): add index request class

---
+++ app/Http/Requests/OccurrenceSpecies/Index.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Http\Requests\OccurrenceSpecies;
+
+use Illuminate\Validation\Rule;
+use VOSTPT\Filters\OccurrenceSpeciesFilter;
+use VOSTPT\Http\Requests\Request;
+
+class Index extends Request
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function rules(): array
+    {
+        return [
+            'page.number' => [
+                'integer',
+            ],
+            'page.size' => [
+                'integer',
+            ],
+            'search' => [
+                'string',
+            ],
+            'exact' => [
+                'boolean',
+            ],
+            'families.*' => [
+                Rule::exists('occurrence_families', 'id'),
+            ],
+            'sort' => [
+                Rule::in(OccurrenceSpeciesFilter::getSortableColumns()),
+            ],
+            'order' => [
+                Rule::in(OccurrenceSpeciesFilter::getOrderValues()),
+            ],
+        ];
+    }
+}
