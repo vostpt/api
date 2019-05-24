8d200778e9c02f42bfe8637140b59eeefd39387b
Quetzy Garcia <quetzyg@altek.org>
2019-05-24 21:31:36 +0100

    feat(OccurrenceSpecies): add controller

---
+++ app/Http/Controllers/OccurrenceSpeciesController.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use VOSTPT\Filters\Contracts\OccurrenceSpeciesFilter;
+use VOSTPT\Http\Requests\OccurrenceType\Index;
+use VOSTPT\Http\Serializers\OccurrenceSpeciesSerializer;
+use VOSTPT\Models\OccurrenceSpecies;
+use VOSTPT\Repositories\Contracts\OccurrenceSpeciesRepository;
+
+class OccurrenceSpeciesController extends Controller
+{
+    /**
+     * Index OccurrenceSpecies.
+     *
+     * @param Index                       $request
+     * @param OccurrenceSpeciesFilter     $filter
+     * @param OccurrenceSpeciesRepository $occurrenceSpeciesRepository
+     *
+     * @throws \OutOfBoundsException
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(
+        Index $request,
+        OccurrenceSpeciesFilter $filter,
+        OccurrenceSpeciesRepository $occurrenceSpeciesRepository
+    ): JsonResponse {
+        $filter->setSortColumn($request->input('sort', $filter->getSortColumn()))
+            ->setSortOrder($request->input('order', $filter->getSortOrder()))
+            ->setPageNumber((int) $request->input('page.number', $filter->getPageNumber()))
+            ->setPageSize((int) $request->input('page.size', $filter->getPageSize()));
+
+        if ($request->has('search')) {
+            $filter->withSearch($request->input('search'), (bool) $request->input('exact', false));
+        }
+
+        if ($request->has('families')) {
+            $filter->withFamilies(...$request->input('families', []));
+        }
+
+        $paginator = $this->createPaginator(OccurrenceSpecies::class, $occurrenceSpeciesRepository->createQueryBuilder(), $filter);
+
+        return response()->paginator($paginator, new OccurrenceSpeciesSerializer(), [
+            'family',
+        ]);
+    }
+}
