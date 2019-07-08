692f3d66a2601acb8899e353deb3985a57fa3df2
Quetzy Garcia <quetzyg@altek.org>
2019-07-08 12:25:10 +0100

    feat(OccurrenceStatuses): add index request class

---
+++ app/Http/Requests/OccurrenceStatus/Index.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Http\Requests\OccurrenceStatus;
+
+use Illuminate\Validation\Rule;
+use VOSTPT\Filters\OccurrenceStatusFilter;
+use VOSTPT\Http\Requests\Request;
+
+class Index extends Request
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function rules(): array
+    {
+        return [
+            'page.number' => [
+                'integer',
+            ],
+            'page.size' => [
+                'integer',
+            ],
+            'search' => [
+                'string',
+            ],
+            'exact' => [
+                'boolean',
+            ],
+            'sort' => [
+                Rule::in(OccurrenceStatusFilter::getSortableColumns()),
+            ],
+            'order' => [
+                Rule::in(OccurrenceStatusFilter::getOrderValues()),
+            ],
+        ];
+    }
+}
