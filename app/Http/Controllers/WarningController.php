40c0db3106abab146230fcb784ba3b50d2ab9803
Quetzy Garcia <quetzyg@altek.org>
2019-07-30 14:23:37 +0100

    feat(Warnings): update controller

---
+++ app/Http/Controllers/WarningController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Collection;
+use VOSTPT\Http\Requests\Warning\Index;
+use VOSTPT\Http\Serializers\WarningSerializer;
+
+class WarningController extends Controller
+{
+    /**
+     * Index IPMA warnings.
+     *
+     * @param Index $request
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Index $request): JsonResponse
+    {
+        return response()->collection(new Collection($this->cache->get('ipma_warnings', [])), new WarningSerializer(), [
+            'county',
+            'county.district',
+        ]);
+    }
+}
--- routes/api/ipma.php
+++ routes/api/ipma.php
@@ -3,18 +3,18 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use VOSTPT\Http\Controllers\IpmaWarningController;
+use VOSTPT\Http\Controllers\WarningController;
 
 /*
 |--------------------------------------------------------------------------
-| District endpoints: /v1/districts/*
+| District endpoints: /v1/ipma/*
 |--------------------------------------------------------------------------
 |
 */
 
 Route::prefix('v1/ipma')->name('ipma::')->group(function () {
     Route::get('/warnings', [
-        IpmaWarningController::class,
+        WarningController::class,
         'index',
     ])->name('warnings::index');
 });
