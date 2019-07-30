6ea85d526914b578d93b621facd24efe45fdf168
Quetzy Garcia <quetzyg@altek.org>
2019-07-30 14:19:09 +0100

    refactor(Warnings): fetcher logic

--- app/Jobs/Warnings/WarningFetcher.php
+++ app/Jobs/Warnings/WarningFetcher.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 namespace VOSTPT\Jobs\Warnings;
 
 use Carbon\Carbon;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Cache\Repository as Cache;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Cache;
 use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
 use VOSTPT\Models\County;
@@ -23,7 +22,7 @@ class WarningFetcher implements ShouldQueue
     use Queueable;
     use SerializesModels;
 
-    const ALLOWED_AWARENESS_LEVELS = [
+    private const ALLOWED_AWARENESS_LEVELS = [
         'yellow',
         'red',
     ];
@@ -98,63 +97,38 @@ class WarningFetcher implements ShouldQueue
     }
 
     /**
-     * Fetch Ipma warnings.
+     * Fetch IPMA warnings.
      *
      * @return bool
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     *
      */
     private function fetchIpmaWarnings(): bool
     {
-        $this->logger->info('Fetching Ipma warnings...');
+        $this->logger->info('Fetching IPMA warnings...');
 
-        $response = $this->serviceClient->getWarnings();
+        try {
+            $response = $this->serviceClient->getWarnings();
+        } catch (GuzzleException $exception) {
+            $this->logger->error($exception->getMessage());
 
-        $ipmaWarnings = collect($response['data'])
-            ->filter(function ($value) {
-                if (! empty($value['awarenessLevelID'])) {
-                    $awarenessLevelId = \mb_strtolower($value['awarenessLevelID']);
-                    $awarenessIsAllowed = \in_array($awarenessLevelId, self::ALLOWED_AWARENESS_LEVELS, true);
-                    if ($awarenessIsAllowed) {
-                        return $value;
-                    }
-                }
-            })->map(function ($item) {
-                // get aux data
-                $countyId = Arr::get(self::AREA_CODES_MAP, $item['idAreaAviso']);
-                $county = County::find($countyId);
-
-                // set/modify attributtes
-                $item['id'] = Uuid::uuid4()->toString();
-                $item['district_id'] = $county->district->id;
-                $item['county_id'] = $county->id;
-                $item['started_at'] = $this->carbonise($item['startTime'])->toDateTimeString();
-                $item['ended_at'] = $this->carbonise($item['endTime'])->toDateTimeString();
-
-                return $item;
-            });
-
-        if (Cache::has('ipma_warnings')) {
-            Cache::forget('ipma_warnings');
+            return false;
         }
 
-        Cache::forever('ipma_warnings', $ipmaWarnings);
+        $warnings = collect($response['data'])
+            ->filter(function (array $warning) {
+                return \in_array($warning['awarenessLevelID'], self::ALLOWED_AWARENESS_LEVELS, true);
+            })->map(function (array $warning) {
+                return \array_merge($warning, [
+                    'id'         => Uuid::uuid4()->toString(),
+                    'county'     => County::find(self::COUNTY_MAPPER[$warning['idAreaAviso']]),
+                    'started_at' => Carbon::parse($warning['startTime']),
+                    'ended_at'   => Carbon::parse($warning['endTime']),
+                ]);
+            });
+
+        $this->cache->forever('ipma_warnings', $warnings);
 
         $this->logger->info('...done!');
 
         return true;
-    }
-
-    /**
-     * @param string|null $dateTime
-     * @return Carbon|null
-     */
-    private function carbonise(string $dateTime = null): ?Carbon
-    {
-        if ($dateTime === null) {
-            return null;
-        }
-
-        return Carbon::parse($dateTime);
     }
 }
