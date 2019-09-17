eb53418306d8b17e3ae968621ad1d36021ac0f53
Quetzy Garcia <quetzyg@altek.org>
2019-09-17 14:23:13 +0100

    test(IPMA): add coverage for surface observation/warning fetch commands

---
+++ tests/Integration/Commands/Ipma/SurfaceObservationFetchCommandTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Tests\Integration\Controllers\UserController;
+
+use GuzzleHttp\Client;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Psr\Log\Test\TestLogger;
+use VOSTPT\Models\WeatherObservation;
+use VOSTPT\Models\WeatherStation;
+use VOSTPT\Tests\Integration\HttpClientMocker;
+use VOSTPT\Tests\Integration\TestCase;
+
+class SurfaceObservationFetchCommandTest extends TestCase
+{
+    use HttpClientMocker;
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function itSuccessfullyFetchesSurfaceObservations(): void
+    {
+        $response = $this->createHttpResponse('tests/data/Ipma/SurfaceObservations.json');
+
+        $this->app->instance(Client::class, $this->createHttpClient($response));
+
+        $this->app->instance(LoggerInterface::class, new NullLogger());
+
+        $station = factory(WeatherStation::class)->create([
+            'serial' => '123',
+            'entity' => 'IPMA',
+        ]);
+
+        factory(WeatherObservation::class)->create([
+            'station_id' => $station->getKey(),
+            'timestamp'  => '1975-07-25 16:25:00',
+        ]);
+
+        $this->assertDatabaseMissing('weather_observations', [
+            'station_id' => 1,
+            'timestamp'  => '1981-12-16 03:33:00',
+        ]);
+
+        $this->artisan('ipma:fetch:surface-observations');
+
+        $this->assertDatabaseHas('weather_observations', [
+            'station_id' => 1,
+            'timestamp'  => '1981-12-16 03:33:00',
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function itFailsToFetchSurfaceObservations(): void
+    {
+        $response = $this->createHttpResponse(null, 404);
+
+        $this->app->instance(Client::class, $this->createHttpClient($response));
+
+        $this->app->instance(LoggerInterface::class, new TestLogger());
+
+        $this->artisan('ipma:fetch:surface-observations');
+
+        $logger = $this->app[LoggerInterface::class];
+
+        $this->assertTrue($logger->hasRecordThatContains('Client error: `GET http://api.ipma.pt/open-data/observation/meteorology/stations/observations.json` resulted in a `404 Not Found`', 'error'));
+    }
+}
---
+++ tests/Integration/Commands/Ipma/WarningFetchCommandTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Tests\Integration\Controllers\UserController;
+
+use GuzzleHttp\Client;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Cache;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Psr\Log\Test\TestLogger;
+use VOSTPT\Models\County;
+use VOSTPT\Tests\Integration\HttpClientMocker;
+use VOSTPT\Tests\Integration\TestCase;
+
+class WarningFetchCommandTest extends TestCase
+{
+    use HttpClientMocker;
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function itSuccessfullyFetchesSurfaceObservations(): void
+    {
+        $response = $this->createHttpResponse('tests/data/Ipma/Warnings.json');
+
+        $this->app->instance(Client::class, $this->createHttpClient($response));
+
+        $this->app->instance(LoggerInterface::class, new NullLogger());
+
+        $this->assertTrue(Cache::missing('ipma_warnings'));
+
+        factory(County::class)->create([
+            'code' => '030300',
+        ]);
+
+        $this->artisan('ipma:fetch:warnings');
+
+        $this->assertTrue(Cache::has('ipma_warnings'));
+    }
+
+    /**
+     * @test
+     */
+    public function itFailsToFetchSurfaceObservations(): void
+    {
+        $response = $this->createHttpResponse(null, 404);
+
+        $this->app->instance(Client::class, $this->createHttpClient($response));
+
+        $this->app->instance(LoggerInterface::class, new TestLogger());
+
+        $this->artisan('ipma:fetch:warnings');
+
+        $logger = $this->app[LoggerInterface::class];
+
+        $this->assertTrue($logger->hasRecordThatContains('Client error: `GET http://api.ipma.pt/json/warnings_www.json` resulted in a `404 Not Found`', 'error'));
+    }
+}
