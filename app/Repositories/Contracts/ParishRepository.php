aa217e2802fbe9ce866ecec0dfe1a7851a1420bd
Quetzy Garcia <quetzyg@altek.org>
2019-09-18 10:57:17 +0100

    feat(ParishRepository): add findByCode() method

--- app/Repositories/Contracts/ParishRepository.php
+++ app/Repositories/Contracts/ParishRepository.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace VOSTPT\Repositories\Contracts;
 
+use VOSTPT\Models\Parish;
+
 interface ParishRepository extends Repository
 {
-    //
+    /**
+     * Find a Parish by code.
+     *
+     * @param string $code
+     *
+     * @return Parish
+     */
+    public function findByCode(string $code): ?Parish;
 }
