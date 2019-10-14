ebb8cd741e42a223d6c99d8b0012d4b4034f6352
Quetzy Garcia <quetzyg@altek.org>
2019-10-14 13:08:54 +0100

    feat(IPMA): inject cache repository instance

--- app/Http/Controllers/IpmaWarningController.php
+++ app/Http/Controllers/IpmaWarningController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace VOSTPT\Http\Controllers;
 
+use Illuminate\Contracts\Cache\Repository as Cache;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Collection;
 use VOSTPT\Http\Requests\Warning\Index;
@@ -15,14 +16,14 @@ class IpmaWarningController extends Controller
      * Index IPMA warnings.
      *
      * @param Index $request
-     *
+     * @param Cache $cache
      * @throws \Psr\SimpleCache\InvalidArgumentException
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index(Index $request): JsonResponse
+    public function index(Index $request, Cache $cache): JsonResponse
     {
-        return response()->collection(new Collection($this->cache->get('ipma_warnings', [])), new WarningSerializer(), [
+        return response()->collection(new Collection($cache->get('ipma_warnings', [])), new WarningSerializer(), [
             'county',
             'county.district',
         ]);
