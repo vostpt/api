9f9b077057cebd8c3025db32701f119524a67c11
Quetzy Garcia <quetzyg@altek.org>
2019-10-22 23:07:55 +0100

    test(Commands): add coverage for API response cache bust command

---
+++ tests/Integration/Commands/Api/ResponseCacheBustCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Tests\Integration\Commands\Ipma;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Cache;
+use VOSTPT\Tests\Integration\HttpClientMocker;
+use VOSTPT\Tests\Integration\TestCase;
+
+class ResponseCacheBustCommandTest extends TestCase
+{
+    use HttpClientMocker;
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function itDoesNotBustsResponseCache(): void
+    {
+        $this->assertFalse(Cache::has('tags_for_cache_busting'));
+
+        $this->artisan('api:response-cache:bust');
+
+        $this->assertFalse(Cache::has('tags_for_cache_busting'));
+    }
+
+    /**
+     * @test
+     */
+    public function itSuccessfullyBustsResponseCache(): void
+    {
+        $this->assertFalse(Cache::has('tags_for_cache_busting'));
+
+        add_cache_bust_tag('foo');
+
+        $this->assertTrue(Cache::has('tags_for_cache_busting'));
+
+        $this->artisan('api:response-cache:bust');
+
+        $this->assertFalse(Cache::has('tags_for_cache_busting'));
+    }
+}
