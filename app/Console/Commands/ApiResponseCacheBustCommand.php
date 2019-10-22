f2cc2b746838c6699fd3bf4a52e7449189e4d68e
Quetzy Garcia <quetzyg@altek.org>
2019-10-22 22:02:11 +0100

    feat(Commands): add API response cache buster command class

---
+++ app/Console/Commands/ApiResponseCacheBustCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VOSTPT\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Bus\Dispatcher as BusDispatcher;
+use VOSTPT\Jobs\Api\ResponseCacheBuster;
+
+class ApiResponseCacheBustCommand extends Command
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $signature = 'api:response-cache:bust';
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $description = 'Bust API response cache';
+
+    /**
+     * Execute the console command.
+     *
+     * @param BusDispatcher $dispatcher
+     *
+     * @return mixed
+     */
+    public function handle(BusDispatcher $dispatcher)
+    {
+        return $dispatcher->dispatchNow(new ResponseCacheBuster());
+    }
+}
