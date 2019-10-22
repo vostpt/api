d6eb85cfb173a2e418734976c22d7819e3f2c28d
Quetzy Garcia <quetzyg@altek.org>
2019-10-22 21:58:38 +0100

    feat(helpers): add functions to keep track of tags for cache busting

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -23,3 +23,58 @@ if (! \function_exists('joined')) {
         return false;
     }
 }
+
+if (! \function_exists('get_cache_bust_tags')) {
+
+    /**
+     * Get the current cache bust tags.
+     *
+     * @throws Exception
+     *
+     * @return array
+     */
+    function get_cache_bust_tags(): array
+    {
+        return cache('tags_for_cache_busting', []);
+    }
+}
+
+if (! \function_exists('add_cache_bust_tag')) {
+
+    /**
+     * Add a cache bust tag.
+     *
+     * @param string $tag
+     *
+     * @throws Exception
+     *
+     * @return bool
+     */
+    function add_cache_bust_tag(string $tag): bool
+    {
+        $tags = \array_unique(get_cache_bust_tags());
+
+        if (\in_array($tag, $tags, true)) {
+            return true;
+        }
+
+        return cache()->forever('tags_for_cache_busting', \array_merge($tags, [
+            $tag,
+        ]));
+    }
+}
+
+if (! \function_exists('clear_cache_bust_tags')) {
+
+    /**
+     * Clear the cache bust tags.
+     *
+     * @throws Exception
+     *
+     * @return bool
+     */
+    function clear_cache_bust_tags(): bool
+    {
+        return cache()->forget('tags_for_cache_busting');
+    }
+}
