46bfde6ecb782cf49281c4e0550534137df5b1ba
Quetzy Garcia <quetzyg@altek.org>
2020-01-04 22:29:41 +0000

    fix(ProCiv): cURL error 52: Empty reply from server by adding user agent header when requesting data

--- app/ServiceClients/ProCivWebsiteServiceClient.php
+++ app/ServiceClients/ProCivWebsiteServiceClient.php
@@ -12,7 +12,8 @@ class ProCivWebsiteServiceClient extends ServiceClient implements Contracts\ProC
     public function getDefaultHeaders(): array
     {
         return [
-            'Content-Type' => 'application/json',
+            'Content-Type' => 'application/json;charset=utf-8',
+            'User-Agent'   => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:71.0) Gecko/20100101 Firefox/71.0',
         ];
     }
 
